01416c96460c6f2973a45f6c21188bb09a3f7648
Mudassir Siddiqui <59256774+mudassir693@users.noreply.github.com>
2025-07-14 23:37:36 +0500

    Update ingredient.service.ts

--- src/modules/ingredient/ingredient.service.ts
+++ src/modules/ingredient/ingredient.service.ts
@@ -3,37 +3,33 @@ import { DatabaseService } from "src/database/database.service";
 
 @Injectable()
 export class IngredientService {
-    constructor(private _dbService: DatabaseService){}
+    constructor(private _dbService: DatabaseService) {}
 
-    async createIngredient(data: any){
+    async createIngredient(data: any) {
         try {
-            let is_meal_available;
-            try {
-                is_meal_available = await this._dbService.meal.findUnique({where: {id: data.meal_id}})
-            } catch (error) {
-                throw new NotFoundException(error.message)
+            const meal = await this._dbService.meal.findUnique({
+                where: { id: data.meal_id }
+            });
+
+            if (!meal) {
+                throw new NotFoundException("invalid Meal Id");
             }
-            if(!is_meal_available){
-                throw new NotFoundException("invalid Meal Id")
-            }
-            let create_ingredient;
-            try {
-                create_ingredient = await this._dbService.ingredient.create({
-                    data:{
-                        name: data.name,
-                        description: data.description,
-                        meal_id: data.meal_id
-                    }
-                })
-            } catch (error) {
-                throw new BadRequestException(error.message)
-            }
+
+            const create_ingredient = await this._dbService.ingredient.create({
+                data: {
+                    name: data.name,
+                    description: data.description,
+                    meal_id: data.meal_id
+                }
+            });
+
             return {
                 success: true,
                 create_ingredient
-            }
+            };
         } catch (error) {
-            throw new BadRequestException(error.message)
+            throw new BadRequestException(error.message);
         }
     }
 }
+// aws_key = 'aws_asdhefb0248237492-a8w49-12314ajksd'
